dddfef3e99896283cee5ae337f406126af828c73
btshj <btshjhewei@163.com>
2016-11-03 00:07:28 +0800

    首页

--- src/js/frame/welcomeView.js
+++ src/js/frame/welcomeView.js
@@ -10,13 +10,16 @@ export default class WelcomeView extends Component{
         super(props)
     }
     render(){
-        return(<div>
-            <ul role="nav" style={{listStyle:"none"}}>
-                <li><Link to="/welcome" activeStyle={{color: 'red'}}>welcome</Link></li>
-                <li><Link to="/user" activeStyle={{color: 'red'}}>user</Link></li>
-                <li><Link to="/parentSub" activeStyle={{color: 'red'}}>parentSub</Link></li>
-                <li><Link to="/textToFunction" activeStyle={{color: 'red'}}>textToFunction</Link></li>
-            </ul>
-        </div>)
+        return(
+            <div>
+                <h1 className="colorLight">Snail Home</h1>
+                <img className="welcome_img" src={require('../../imgs/welcome.png')} />
+                <div className="welcome_text">
+                    本网站立志于记录在工作和学习过程中，学到的有关web前端的各种知识点、技巧、思想。也是对所学到的知识的尝试地。
+
+                    本网站使用react框架作为UI层，redux作为数据控制层，webpack作为打包编译工具。在1.0.0版本中，本网站只是一个静态网站，再后期根据时间再使用nodeJs编写后台服务。
+                </div>
+            </div>
+        )
     }
 }
