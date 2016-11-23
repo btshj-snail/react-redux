361bde1263222c522f8bf30e929c747b23c29e1c
snail <snail>
2016-11-23 14:13:08 +0800

    'tijiao'

--- src/js/modules/krpanpoOfficialExample/krpanoOfficialExample.js
+++ src/js/modules/krpanpoOfficialExample/krpanoOfficialExample.js
@@ -1,3 +1,32 @@
 /**
  * Created by snail on 2016/11/22.
  */
+
+import React,{Component} from 'react';
+
+export default class KrpanoOfficialExample extends Component{
+
+    componentDidMount(){
+        // embedpano({
+        //     swf:"/third_component/krpanpo/player.swf",
+        //     xml:"/third_component/krpanpo/videopano.xml",
+        //     target:"pano",
+        //     html5:"auto",
+        //     passQueryParameters:true,
+        //     onready:function(krpano){
+        //         console.log(krpano);
+        //     }
+        // });
+
+        embedpano({xml:"/third_component/krpanpoOfficialExample/example.xml", swf:"/third_component/krpanpo/player.swf", target:"pano", html5:"false", mobilescale:1.0, passQueryParameters:true});
+    }
+
+
+    render(){
+        return (
+            <div id="pano" style={{width:'100%',height:'600px'}}>
+
+            </div>
+        )
+    }
+}
